747a19c5111bfc659568514deb1dd8a027b1e61b
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-09 16:51:19 +0000

    Added preprocessing script

---
+++ preprocessing.py
@@ -0,0 +1,7 @@
+"""
+This script takes in a archive folder produced by github-backup and 
+creates a folder with a issue content per files, where the text was preprocessed
+to strip markdown and 
+
+
+"""
